11073e17f148121306bf0a5668659f8ad8f58f65
starshun-prog <kohaku1015game@gmail.com>
2022-06-17 01:29:34 +0900

    remove src/Admin remove need not initial import from MenuBar

--- vite-firebase-react/src/Component/Basic/MenuBar.jsx
+++ vite-firebase-react/src/Component/Basic/MenuBar.jsx
@@ -1,25 +1,18 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import { Button } from '@mui/material';
-import { auth } from '../../firebase';
-import { signOut } from 'firebase/auth';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Button } from "@mui/material";
+import { auth } from "../../firebase";
+import { signOut } from "firebase/auth";
 
 const MenuBar = () => {
-
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color='secondary'>
+      <AppBar position="static" color="secondary">
         <Toolbar>
           <IconButton
             size="large"
@@ -30,27 +23,31 @@ const MenuBar = () => {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: "center" }}>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, textAlign: "center" }}
+          >
             お問い合わせ一覧
           </Typography>
           {auth && (
             <div>
-            <Button
-              variant='contained'
-              sx={{
-                margin: "20px",
-                zIndex: "100",
-                position: "relative"
-              }}
-              onClick={async () => await signOut(auth)}
-            >
-              logout
-            </Button>
+              <Button
+                variant="contained"
+                sx={{
+                  margin: "20px",
+                  zIndex: "100",
+                  position: "relative",
+                }}
+                onClick={async () => await signOut(auth)}
+              >
+                logout
+              </Button>
             </div>
           )}
         </Toolbar>
       </AppBar>
     </Box>
   );
-}
+};
 export default MenuBar;
